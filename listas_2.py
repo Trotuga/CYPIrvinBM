478e7eaa06d15d9efb36558bb6bfb49e0f5a86ec
trotuga <irvish212211@gmail.com>
2019-11-05 17:51:35 -0600

    aa

---
+++ listas_2.py
@@ -0,0 +1,6 @@
+autos = ["Akura", "Honda", "Toyota", "Nissan"]
+motos = ["Ducati", "Kawasaki", "Harley Davison", "BMW", "Italika"]
+bicis = ["Benoto", "BMX", "Apache","Mercurio", "Treck"]
+vehiculos = [autos, motos, bicis]
+
+print(motos[2])
